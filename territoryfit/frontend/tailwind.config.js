2c4f671a7a4e16c9588dad2d68fd03c8c900f539
Kunal <kunalkaushikgkk@gmail.com>
2026-04-06 22:51:39 +0530

    Update tailwind.config.js

--- territoryfit/frontend/tailwind.config.js
+++ territoryfit/frontend/tailwind.config.js
@@ -2,7 +2,7 @@ import typography from '@tailwindcss/typography';
 import containerQueries from '@tailwindcss/container-queries';
 import animate from 'tailwindcss-animate';
 
-/** @type {import('tailwindcss').Config} */
+/** @type {import('tailwindcss').Config} */ 
 export default {
   darkMode: ['class'],
   content: ['index.html', 'src/**/*.{js,ts,jsx,tsx,html,css}'],
